ac11f7ad06e825aeb4624adc0a70d01c64690393
Henrique Miranda <henrique@disroot.org>
2021-03-27 19:00:35 -0300

    fix static

--- pynerds_site/urls.py
+++ pynerds_site/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import home
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
